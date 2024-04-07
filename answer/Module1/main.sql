7428b4fde6a0ea141fb6c2429dda6c0d87d906fa
MaximPortnov <portmaxalex@mail.ru>
2024-04-07 16:01:15 +0700

    fix main.sql in answer(module1)

--- answer/Module1/main.sql
+++ answer/Module1/main.sql
@@ -1,18 +1,17 @@
--- Active: 1712235013311@@localhost@5432@test@public
-
+-- Task 1
 SELECT name, age 
 FROM customer
 WHERE address = 'Kazan';
 
 
-
+-- Task 2
 SELECT name, age 
 FROM customer
 WHERE address = 'Kazan' AND gender = 'female'
 ORDER BY name;
 
 
-
+--Task 3
 SELECT name, rating 
 FROM cafe
 WHERE rating >= 3.5 AND rating <= 5.0
@@ -24,9 +23,7 @@ WHERE rating BETWEEN 3.5 AND 5.0
 ORDER BY rating;
 
 
-
-
-
+-- Task 4
 SELECT DISTINCT cv.customer_id 
 FROM customer_visits as cv
 WHERE 
@@ -35,8 +32,7 @@ WHERE
 ORDER BY cv.customer_id DESC;
 
 
-
-
+-- Task 5
 SELECT 
     name || 
     ' (age:' || age || 
@@ -47,8 +43,7 @@ FROM customer
 ORDER BY customer_information ASC;
 
 
-
-
+-- Task 6
 SELECT 
     (SELECT name FROM customer WHERE id = co.customer_id ) AS name
 FROM customer_orders AS co
@@ -59,11 +54,8 @@ WHERE co.order_date = '2022-01-07'
         co.menu_id = 18
     );
 
-    
 
-
-
-
+-- Task 7
 SELECT 
     (SELECT name FROM customer WHERE id = co.customer_id ) AS name,
     (SELECT name FROM customer WHERE id = co.customer_id ) = 'Denis' AS check_name 
@@ -76,9 +68,7 @@ WHERE co.order_date = '2022-01-07'
     );
 
 
-
-
-
+-- Task 8
 SELECT 
     c.id, 
     c.name, 
@@ -91,18 +81,14 @@ FROM customer AS c
 ORDER BY interval_info ASC;
 
 
-
-
-
-SELECT 
-    * 
+-- Task 9
+SELECT * 
 FROM customer_orders AS co
 WHERE co.id % 2 = 0
 ORDER BY id;
 
 
-
-
+-- Task 10
 SELECT 
     (SELECT name FROM customer WHERE id = cv.customer_id) AS customer_name,
     (SELECT name FROM cafe WHERE id = cv.cafe_id) AS cafe_name
